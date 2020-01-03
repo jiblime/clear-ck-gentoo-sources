083f45d5bbe5a27217d74f783dc69b672137a537
jiblime <47689567+jiblime@users.noreply.github.com>
2020-01-02 23:16:04 -0800

    5.4.0-pf3 kernel (5.4.6), updated to 5.4.7
    Excluded patches from Clear Linux for either compat or because it is already patched:
    *-driver-core-add-dev_groups-to-all-drivers.patch
    *-Revert-iwlwifi-assign-directly-to-iwl_trans-cfg-in-Q.patch
    
    Excluded patches from the usual series because it is already patched:
    misc/001-iwd-rfkill_5.4.patch
    
    Regarding WireGuard not compiling: it has to do with the pf kernel patchset preferring ipv6_dst_lookup. Have yet to nail it down
    
    Patches included:
    v5.4...v5.4-pf3.diff
    
    genpatches/1500_XATTR_USER_PREFIX.patch
    genpatches/1510_fs-enable-link-security-restrictions-by-default.patch
    genpatches/2000_BT-Check-key-sizes-only-if-Secure-Simple-Pairing-enabled.patch
    genpatches/2500_usb-storage-Disable-UAS-on-JMicron-SATA-enclosure.patch
    genpatches/2600_enable-key-swapping-for-apple-mac.patch
    genpatches/4567_distro-Gentoo-Kconfig.patch
    
    misc/000-experimental-x86-64-opts.patch
    
    misc/003-better-it87-detection_5.4.patch
    
    misc/004-async-futex1_5.4.patch
    
    misc/005-async-futex2_5.4.patch
    
    misc/006-treewide-awk-5.0-over-escaping-fix_5.4.patch
            This needs to be redone, some over-escaping is missing
    
    misc/008-Add-git-version-of-zenpower-as-a-builtin-module.patch
    
    submodules/.generated/clear.patch
            If you have a patched binutils use the other patch generator to get better performance for 32bit junk
    
    misc/009-linux-acs-override_5.4.2.patch
    Source: https://queuecumber.gitlab.io/linux-acs-override/
    Autobuilds haven't changed so I haven't updated the revision number
    
    5.4.7_incremental.patch

--- patch-generator-with-binutils-opt.sh
+++ patch-generator-with-binutils-opt.sh
@@ -121,9 +121,9 @@ cl_distro+="*-staging-exfat-add-exfat-filesystem-code-to-staging.patch|"
 # I haven't looked into it but I think this has been upstreamed already
 cl_distro+="*-driver-core-add-dev_groups-to-all-drivers.patch|"
 # Doesn't patch
-cl_distro+="*-Revert-iwlwifi-assign-directly-to-iwl_trans-cfg-in-Q.patch|"
+cl_distro+="*-Revert-iwlwifi-assign-directly-to-iwl_trans-cfg-in-Q.patch"
 # Doesn't need to be reverted?
-cl_distro+="*-add-workaround-for-binutils-optimization.patch"
+#cl_distro+="*-add-workaround-for-binutils-optimization.patch"
 # x86_64-pc-linux-gnu/bin/as: unrecognized option '-mbranches-within-no-boundaries'
 # Patches to recompile binutils with here: https://github.com/clearlinux-pkgs/binutils
 
