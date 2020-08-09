7aaab87abd67dde8fd55a7b48e8e8bcf5e295913
jiblime <47689567+jiblime@users.noreply.github.com>
2020-08-09 13:06:03 -0700

    Include lowercase naming for excluded patches
    Not entirely complete. . .
    Probably better to pipe to a file to parse?

--- patch-generator.sh
+++ patch-generator.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-update_mods() {
+# Clear Linux kernel repository:
+#	https://github.com/clearlinux-pkgs/linux
+
+# zenpower author:
+#	https://github.com/ocerman
+#	For use with https://github.com/ocerman/zenmonitor
+
+update_mods()
+{
+
 	git submodule update --force --remote --quiet
 
 	echo -e "\nZenpower: "$(git -C submodules/zenpower describe --tags)"\nCommit date: "$(git -C submodules/zenpower show -s --format=%ci)"\c"
 	echo -e "\nClear Linux: "$(git -C submodules/clear describe --tags)"\nCommit date: "$(git -C submodules/clear show -s --format=%ci)"\c"
 	echo -e "\n---\n"
 }
-update_mods
+
+
+zen_module()
+{
 
 # For Ryzen 3000 voltage/temp/clock detection.
 
@@ -18,125 +30,64 @@ if [[ -f submodules/zenpower/zenpower.c ]] ; then
 	sed -i 's/^/+/g' submodules/.zenpower-skel/zenpower.potch
 	cat submodules/.zenpower-skel/zenpower.potch  >> misc/006-Add-git-version-of-zenpower-as-a-builtin-module.patch
 fi
-#	ls --color=always misc/008-Add-git-version-of-zenpower-as-a-builtin-module.patch
-#	echo -e "Created the freshest zenpower built in module available, courtesy of:"
-#	echo -e "https://github.com/ocerman. Best used with https://github.com/ocerman/zenmonitor"
-#	echo -e "If you do not have a Ryzen processor, disable CONFIG_SENSORS_ZENPOWER"
 
-# Below, Clear patches are categorized and then combined all into one so the series file doesn't need adjustment
-
-# IDEAS: Use case statements instead
-# 	Identify and exclude patches that reduce performance in lieu of security
-#	Create an additional prompt for Intel exclusive patches that have been excluded by default
-#	Separate anything performance related into its own section
-
-#version=$(cat submodules/clear/upstream | sed 's/^.*\-//g; s/\.tar\.xz//g')
-#echo -e "Clear Linux's patches for $version.\n"
-
-cve-patches()
-{
-CVE=$(cd submodules/clear; ls | grep '^CVE.*\.patch'; cd $OLDPWD)
-
-if [[ -z "${CVE}" ]] ; then
-	echo -e "No CVE patches to apply, continuing\n"
-	return
-else
-	echo -e "CVE patches\n${CVE}\n"
-	create_cve
-fi
-}
-
-create_cve()
-{
-echo -e "CVE patches are patches made to fix security issues in the kernel"
-read -p "Include CVE patches?[Y/n] " create_cve
-echo -e "User generated at: $(date)" > submodules/.generated/0001-CL-CVE.patch
-echo -e "Tag: $(git -C submodules/clear describe --tags)"
-
-case "${create_cve}" in
-	[Yy]* | '')
-		cat submodules/clear/CVE*.patch >> submodules/.generated/0001-CL-CVE.patch && echo -e "\n\e[32mAdded CVE patches.\e[0m\n"
-		;;
-	[Nn]*)
-		echo -e "\n\e[31mNot adding CVE patches and removing older patches.\e[0m\n";
-		rm submodules/.generated/0001-CL-CVE.patch
-		;;
-	*)
-		echo "Input unrecognized..."; create_cve
-esac
 }
 
 
 cl-patches()
 {
-shopt -s extglob
 
-#  Excluded Clear Linux patches
+#version=$(cat submodules/clear/upstream | sed 's/^.*\-//g; s/\.tar\.xz//g')
+#echo -e "Clear Linux's patches for $version.\n"
 
-cl_distro="*-bootstats-add-printk-s-to-measure-boot-time-in-more-.patch|"
+#  /Excluded Clear Linux patches
+
 # > Few distro-tweaks to add printk's to visualize boot time better
-# cl_distro+="*-init-wait-for-partition-and-retry-scan.patch|"
-# Adds a wait period for Clear Linux because it boots too fast
-cl_distro+="*-Add-boot-option-to-allow-unsigned-modules.patch|"
+cl_distro+="[bB]ootstats-add-printk-s-to-measure-boot-time-in-more-.patch\|"
+
 # Adds option to allow unsigned modules when Secure Boot is off
-cl_distro+="*-Enable-stateless-firmware-loading.patch|"
+cl_distro+="[aA]dd-boot-option-to-allow-unsigned-modules.patch\|"
+
 # Prefers firmware from the (Clear Linux's stateless) user directories first
-cl_distro+="*-Migrate-some-systemd-defaults-to-the-kernel-defaults.patch|"
+cl_distro+="[eE]nable-stateless-firmware-loading.patch\|"
+
 # > These settings are needed to prevent networking issues when the networking modules come up by default without explicit settings
-cl_distro+="*-add-scheduler-turbo3-patch.patch|"
+cl_distro+="[mM]igrate-some-systemd-defaults-to-the-kernel-defaults.patch\|"
+
 # Doesn't work for non CL distros
+cl_distro+="[aA]dd-scheduler-turbo3-patch.patch\|"
 
-cl_distro+="*-iwlwifi-mvm-Do-not-require-PHY_SKU-NVM-section-for-3.patch|"
-
-cl_distro+="*-x86-microcode-Force-update-a-uCode-even-if-the-rev-i.patch|"
 # Intel specific? Unsure of the need for this.
-cl_distro+="*-x86-microcode-echo-2-reload-to-force-load-ucode.patch|"
+cl_distro+="x86-microcode-Force-update-a-uCode-even-if-the-rev-i.patch\|"
 # Same as above.
-cl_distro+="*-fix-bug-in-ucode-force-reload-revision-check.patch|"
+cl_distro+="x86-microcode-echo-2-reload-to-force-load-ucode.patch\|"
 # Same as above.
+cl_distro+="[fF]ix-bug-in-ucode-force-reload-revision-check.patch\|"
 
-#cl_distro+="*-use-lfence-instead-of-rep-and-nop.patch|" ## pause does not serialize on AMD, therefore rep/nop do not either afaik
-# ~~Need to determine if this is already resolved in another way/performance impact.  https://spectreattack.com/spectre.pdf https://newsroom.intel.com/wp-content/uploads/sites/11/2018/01/Intel-Analysis-of-Speculative-Execution-Side-Channels.pdf~~
+# > (DISTRO TWEAK -- NOT FOR UPSTREAM)
+cl_distro+="[iI]ncrease-the-ext4-default-commit-age.patch"
 
-#[[ -z $(gcc -shared -mzero-caller-saved-regs=used /dev/null -o /dev/null) ]] &&
-cl_distro+="*-zero-extra-registers.patch|"
-# Requires GCC patch. https://github.com/clearlinux-pkgs/gcc/blob/master/zero-regs-gcc8.patch
+# ~~Need to determine if this is already resolved in another way/performance impact.
+# https://spectreattack.com/spectre.pdf
+# https://newsroom.intel.com/wp-content/uploads/sites/11/2018/01/Intel-Analysis-of-Speculative-Execution-Side-Channels.pdf
+#cl_distro+="use-lfence-instead-of-rep-and-nop.patch" ## pause does not serialize on AMD, therefore rep/nop do not either(?)
 
-cl_distro+="*-gcc-10-warnings-fix-low-hanging-fruit.patch|"
-cl_distro+="*-Stop-the-ad-hoc-games-with-Wno-maybe-initialized.patch|"
-cl_distro+="*gcc-10*|"
-# In 5.4.41
+#  \Excluded Clear Linux patches
 
-[[ -z $(grep 'ck' './version') ]] &&
-cl_distro+="*-WireGuard-fast-modern-secure-kernel-VPN-tunnel.patch|"
-# pf-kernel merged, ck did not
 
-[[ -z "$(as -mbranches-within-no-boundaries --warn /dev/null >/dev/null 2>&1)" ]] &&
-cl_distro+="*-add-workaround-for-binutils-optimization.patch|"
-# binutils's assembler needs a patch from https://github.com/clearlinux-pkgs/binutils/
-# This patch is no longer included in their binutils or kernel's patchset
-# I could not find "mbranches-within-no-boundaries" nor "OPTION_MBRANCHES_WITH_NO_BOUNDARIES" anywhere on the internet besides GitHub
+# shell-escape should be enough?
+ALL_CLEAR=$(ls --quoting-style=shell-escape submodules/clear/*patch)
+CLEAR=$(echo -n -E "${ALL_CLEAR[@]}" | grep -v "${cl_distro[@]}")
 
-cl_distro+="*-Increase-the-ext4-default-commit-age.patch|"
-# > (DISTRO TWEAK -- NOT FOR UPSTREAM) | Moved to the bottom so the last patch will never have "|" appended
+echo -e "Clear Linux patches:\n"
+echo -n -E "${CLEAR}" |sed 's/^.*clear\///g'
+echo -e "\n"
 
-cl_distro+="archive"
-
-CLEAR=($(cd submodules/clear; ls !(${cl_distro}) | grep -v 'fpga\|^CVE\|.*patch\-\|perfbias' | grep '^.*\.patch'; cd $OLDPWD))
-echo -e "Clear Linux patches"
-printf '%s\n' "${CLEAR[@]}"
-echo # \n
-shopt -u extglob
-
-echo -e "The rest of the patches; this is definitely what you're using Clear Linux patches for\n"
-read -p "Show list of patches excluded for compatibility? (These will not be added if you say yes or no) [yN] " show_ex
+read -p "Show list of excluded patches? [yN] " show_ex
 case "${show_ex}" in
 	[Yy]*)
-		excluded_list=${cl_distro}
-		echo -e "\e[2m"
-		echo "Excluded list:"
-		echo ${excluded_list} | sed 's/|/\n/g'
-		echo -e "\e[22m"
+		comm <(echo "${ALL_CLEAR}") <(echo "${CLEAR}") -3 | sed 's/^.*clear\///g'
+		echo ""
 		;;
 	[Nn]* | *)
 		return
@@ -145,17 +96,17 @@ esac
 
 create_clr()
 {
-read -p "Include the rest of the Clear Linux patches? (Recommended)[Y/n] " create_clr
-echo -e "User generated at: $(date)" > submodules/.generated/0003-CL-CLR.patch
+read -p "Update and overwrite any existing Clear Linux patchset? [Y/n] " create_clr
+echo -e "User generated at: $(date)" > submodules/.generated/clear.patch
 echo -e "Tag: $(git -C submodules/clear describe --tags)"
 
 case "${create_clr}" in
 	[Yy]* | '')
-		warpten && echo -e "\n\e[32mAdded Clear Linux patches.\e[0m\n"
+		warpten
 		;;
 	[Nn]*)
-		echo -e "\n\e[31mNot adding Clear Linux patches and removing older patches.\e[0m"
-		rm submodules/.generated/0003-CL-CLR.patch
+		echo -e "Skipping..."
+		return
 		;;
 	*)
 		echo "Input unrecognized..."; create_clr
@@ -164,18 +115,23 @@ esac
 
 warpten()
 {
+
+CLEAR=($(echo -n -E "${CLEAR}" | tr '\n' ' '))
+
 for ((i=0; i<${#CLEAR[@]}; i++)); do
-	cat submodules/clear/"${CLEAR[i]}" >> submodules/.generated/0003-CL-CLR.patch;
+	cat "${CLEAR[i]}" >> submodules/.generated/clear.patch;
 done
+
+[[ $? != "0" ]] && echo "Something strange happened" || echo "Generated at submodules/.generated/clear.patch"
+
 }
 
 
 
+update_mods
 
+zen_module
 
-# The functions are separate to prevent spamming of patchsets
-cve-patches
 cl-patches && create_clr
 
 
-cat submodules/.generated/00*CL*.patch > submodules/.generated/clear.patch && echo -e "Created new Clear Linux patchset at submodules/.generated/clear.patch."
